550109b6228d6087506a793c4ca937476b630678
chenshuangrui <chenshuangrui@gmail.com>
2025-05-08 18:05:28 +0800

    🐛 Fix summary tool description

--- sdk/nexent/core/tools/summary_tool.py
+++ sdk/nexent/core/tools/summary_tool.py
@@ -15,13 +15,17 @@ default_system_prompt = ("# 你是一个总结专家，你的任务是根据检�
 
 
 class SummaryTool(Tool):
-    name = "summary_search_content"
-    description = "具有可靠的总结能力，可以生成令用户满意的回答。需要传入用户提问与检索工具的检索结果。返回内容可以直接作为final_answer的输入。"
+    name = "summary_content"
+    description = """
+    This is a tool for summarizing content. It can generate a response that satisfies the user. 
+    It requires the user's question and the search result of the search tool. 
+    The returned content should be directly used as the input of final_answer."
+    """
     inputs = {
-        "query": {"type": "string", "description": "输入用户提问"},
-        "search_result": {
+        "query": {"type": "string", "description": "Input the user's question"},
+        "content": {
             "type": "array",
-            "description": "一个包含多个字符串的列表，每个字符串表示之前检索到的**原始信息**，直接给入检索之后的变量，不要擅自总结。"
+            "description": "A list of strings, each representing the **original information** previously retrieved, directly input the variable after retrieval, do not summarize it."
         }
     }
     output_type = "string"
@@ -33,16 +37,16 @@ class SummaryTool(Tool):
         self.system_prompt = system_prompt
         self.running_prompt = "总结生成中..."
 
-    def forward(self, query: str, search_result: List[str]) -> str:
+    def forward(self, query: str, content: List[str]) -> str:
         # 发送工具运行消息
         self.observer.add_message("", ProcessType.TOOL, self.running_prompt)
 
-        search_result_concat = ""
-        for search_str in search_result:
-            search_result_concat += search_str + "\n"
+        result_concat = ""
+        for content_str in content:
+            result_concat += content_str + "\n"
 
         messages = [{"role": MessageRole.SYSTEM, "content": self.system_prompt},
-            {"role": MessageRole.USER, "content": f"### 检索信息：{search_result_concat}\n### 用户提问：{query}\n"}]
+            {"role": MessageRole.USER, "content": f"### Related information：{result_concat}\n### User question：{query}\n"}]
         model_output_message = self.model(messages=messages)
 
         return model_output_message.content
