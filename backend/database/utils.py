58d21b119ab50d799229a66e3290101bc5b29859
linyu <1611890852@qq.com>
2025-05-12 14:39:41 +0800

    ✨Knowledge base summary:Simulate the user id using uuid

--- backend/database/utils.py
+++ backend/database/utils.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from fastapi import Header
+import uuid
 
-
-# mork的假数据，用于获取用户id
+# mock的假数据，用于获取用户id
 def get_current_user_id(authorization: Optional[str] = Header(None)):
-    return "刘亦菲"
+    return str(uuid.uuid4())  # 返回随机生成的UUID字符串
