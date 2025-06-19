9ecf09900f49ec3fa0194668a95cb51f8d59c6d2
Bavichi <909452380@qq.com>
2025-06-19 09:33:20 +0800

    ✨ Administrators manage tenants separately, with each administrator able to create independent Agents and model configurations. #311
    3. Added the tenant_config_manager along with related get/set methods, switching the approach of obtaining model-related configurations from reading .env files to fetching from the database. Cache support was also implemented.
    4. Added a tenant_id filter to the tool_list endpoint, where tools with unrelated tenant_ids will not be visible based on permissions.

---
+++ backend/database/tenant_config_db.py
@@ -0,0 +1,97 @@
+from typing import Optional, Dict, List, Any
+
+from .client import db_client, get_db_session
+from .db_models import TenantConfig
+
+
+def get_all_configs_by_tenant_id(tenant_id: str):
+    with get_db_session() as session:
+        result = session.query(TenantConfig).filter(TenantConfig.tenant_id == tenant_id,
+                                                    TenantConfig.delete_flag == "N").all()
+        record_info = []
+        for item in result:
+            record_info.append({
+                "config_key": item.config_key,
+                "config_value": item.config_value,
+                "tenant_config_id": item.tenant_config_id,
+                "update_time": item.update_time
+            })
+        
+        return record_info
+
+
+def get_tenant_config_info(tenant_id: str, user_id: str, select_key: str):
+    with get_db_session() as session:
+        result = session.query(TenantConfig).filter(TenantConfig.tenant_id == tenant_id,
+                                                    TenantConfig.user_id == user_id,
+                                                    TenantConfig.config_key == select_key,
+                                                    TenantConfig.delete_flag == "N").all()
+        record_info = []
+        for item in result:
+            record_info.append({
+                "config_value": item.config_value,
+                "tenant_config_id": item.tenant_config_id
+            })
+        return record_info
+    
+
+def get_single_config_info(tenant_id: str, user_id: str, select_key: str):
+    with get_db_session() as session:
+        result = session.query(TenantConfig).filter(TenantConfig.tenant_id == tenant_id,
+                                                    TenantConfig.user_id == user_id,
+                                                    TenantConfig.config_key == select_key,
+                                                    TenantConfig.delete_flag == "N").first()
+        record_info = {
+                "config_value": result.config_value,
+                "tenant_config_id": result.tenant_config_id
+            }
+
+        return record_info
+
+
+def insert_config(insert_data: Dict[str, Any]):
+    with get_db_session() as session:
+        try:
+            session.add(TenantConfig(**insert_data))
+            session.commit()
+            return True
+        except Exception as e: 
+            session.rollback()
+            return False
+
+
+def delete_config_by_tenant_config_id(tenant_config_id: int):
+    with get_db_session() as session:
+        try:
+            session.query(TenantConfig).filter(TenantConfig.tenant_config_id == tenant_config_id,
+                                               TenantConfig.delete_flag == "N").update({"delete_flag": "Y"})
+            session.commit()
+            return True
+        except Exception as e:
+            session.rollback()
+            return False
+
+
+def update_config_by_tenant_config_id(tenant_config_id: int, update_value: str):
+    with get_db_session() as session:
+        try:
+            session.query(TenantConfig).filter(TenantConfig.tenant_config_id == tenant_config_id,
+                                               TenantConfig.delete_flag == "N").update({"config_value": update_value})
+            session.commit()
+            return True
+        except Exception as e:
+            session.rollback()
+            return False
+        
+
+def update_config_by_tenant_config_id_and_data(tenant_config_id: int, insert_data: Dict[str, Any]):
+    with get_db_session() as session:
+        try:
+            session.query(TenantConfig).filter(TenantConfig.tenant_config_id == tenant_config_id,
+                                               TenantConfig.delete_flag == "N").update(insert_data)
+            session.commit()
+            return True 
+        except Exception as e:
+            session.rollback()
+            return False
+
