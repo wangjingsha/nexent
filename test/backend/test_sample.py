32acc5dd97ba372dab66bcdf483828e756d9ca9f
jiangpeiling <jiangpeiling@noreply.gitcode.com>
2025-07-09 20:54:06 +0800

    ✨Add codecov report.

--- test/backend/test_sample.py
+++ test/backend/test_sample.py
@@ -4,15 +4,14 @@ import unittest
 def add(a, b):
     return a + b
 
-class TestSample(unittest.TestCase):
-    def test_add_positive(self):
-        self.assertEqual(add(1, 2), 3)
+import importlib
 
-    def test_add_negative(self):
-        self.assertEqual(add(-1, -2), -3)
-
-    def test_add_zero(self):
-        self.assertEqual(add(0, 0), 0)
+class TestConversationManagementAppAdd(unittest.TestCase):
+    def test_add(self):
+        module = importlib.import_module('backend.apps.test')
+        self.assertEqual(module.add(1, 2), 3)
+        self.assertEqual(module.add(-1, -2), -3)
+        self.assertEqual(module.add(0, 0), 0)
 
 if __name__ == '__main__':
     unittest.main() 
